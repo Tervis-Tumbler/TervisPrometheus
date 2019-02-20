88a2d17948fb136350d26f7ce9ae455d075dd533
Chris Magnuson <hourglasssand@gmail.com>
2019-02-20 10:25:18 -0500

    Initial configuration

--- TervisPrometheus.psm1
+++ TervisPrometheus.psm1
@@ -1,3 +1,35 @@
 ﻿function Get-PrometheusNodeNames {
     Get-TervisApplicationNodeNames -Name Prometheus
 }
+
+function Start-Prometheus {
+    docker run -d -p 9090:9090 --name prometheus prom/prometheus:v2.7.1
+}
+
+function Output-PrometheusConfiguration {
+    [PSCustomObject]@{
+        scrape_configs = @(
+            @{
+                job_name = "envoy"
+                metrics_path = "/stats/prometheus"
+                static_configs = @(
+                    @{
+                        targets = @(
+                            "images2.tervis.com:9901"
+                        )
+                    }
+                )
+            }
+        )
+    } |
+    ConvertTo-Yaml |
+    Out-File -FilePath prometheus.yml -Encoding ascii
+}
+
+function Copy-PrometheusConfiguration {
+    docker cp prometheus.yml prometheus:/etc/prometheus/prometheus.yml
+}
+
+function Restart-Prometheus {
+    docker restart prometheus
+}
